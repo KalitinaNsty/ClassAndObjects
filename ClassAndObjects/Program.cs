1945caa6cc714d30d48cfdd503fb77909084bc4b
Анастасия Калитина <calitina.anastasya2010@yandex.ru>
2022-08-05 20:15:09 +0300

    add User

--- ClassAndObjects/Program.cs
+++ ClassAndObjects/Program.cs
@@ -6,7 +6,19 @@ namespace ClassAndObjects
     {
         static void Main(string[] args)
         {
-            
+            User user1 = new User();
+            //user1.login = "qwerty";
+            //user1.password = "12345";
+            //user1.age = 26;
+            user1.Print();
+            user1.SetAge(13);
+            user1.SetLogin("qwerty");
+            user1.SetPassword("234567");
+            user1.Print();
+            int currenAge = user1.GetAge();
+            Console.WriteLine(currenAge);
+            User user2 = new User("pole", "newPass", 10 );
+            user2.Print();
         }
     }
 }
---
+++ ClassAndObjects/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjects
+{
+    class User
+    {
+        private string login;
+        private string password;
+        private int age;
+        public User(string login, string password, int age) 
+        {
+            this.login = login;
+            this.password = password;
+            this.age = age;
+        }
+        public User()
+        {
+
+        }
+        public void SetAge(int age)
+        {
+            if (age < 0)
+            {
+                return;
+            }
+            this.age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{login}, {password}, {age}");
+        }
+        public void SetLogin(string login)
+        {
+            if (login == "" || login == null)
+            {
+                return;
+            }
+            this.login = login;
+        }
+        public void SetPassword(string password)
+        {
+            if (password == null || password == "")
+            {
+                return;
+            }
+            this.password = password;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public string GetLogin()
+        {
+            return login;
+        }
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
